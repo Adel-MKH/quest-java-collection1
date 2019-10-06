ff9c98922213d2f3089b1a651ec8fe3df961fa5c
Adel-MKH <43877949+Adel-MKH@users.noreply.github.com>
2019-10-07 00:50:12 +0200

    Solution

--- Hero.java
+++ Hero.java
@@ -1,30 +1,45 @@
-public class Hero {
+import java.util.*;
 
-    // attributs
-    private String name;
-    private int age;
+public class Thanos {
 
-    // constructeurs
-    public Hero(String name, int age) {
-        this.name = name;
-        this.age = age;
-    }
+    public static void main(String[] args) {
 
-    // accesseurs (getters)
-    public String getName() {
-        return name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    // mutateurs (setters)
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
+    	// TODO 1 : Create an empty heroes list
+    	List<Hero> heroes = new ArrayList<Hero>();
+    	
+    	 // TODO 2 : Add those heroes to the list
+    	Hero blackWidow = new Hero("Black Widow", 34);
+    	Hero captainAmerica = new Hero("Captaine America", 100);
+    	Hero vision = new Hero("Vision", 3);
+    	Hero ironMan = new Hero("Iron Man", 48);
+    	Hero scarletWitch = new Hero("Scarlet Witch", 29);
+    	Hero thor = new Hero("Thor", 1500);
+    	Hero spiderman = new Hero("Spider-Man", 18);
+    	Hero hulk = new Hero("Hulk", 49);
+    	Hero drStrange = new Hero("Doctor Strange", 42);    	
+    	heroes.add(blackWidow);
+    	heroes.add(captainAmerica);
+    	heroes.add(vision);
+    	heroes.add(ironMan);
+    	heroes.add(scarletWitch);
+    	heroes.add(thor);
+    	heroes.add(spiderman);
+    	heroes.add(hulk);
+    	heroes.add(drStrange);
+    	
+    	// TODO 3 : It's Thor birthday, now he's 1501
+    	thor.setAge(1501);
+    	   	
+    	// TODO 4 : Shuffle the heroes list
+    	Collections.shuffle(heroes);
+    	
+    	// TODO 5 : Keep only the half of the list
+    	int halfArray = heroes.size() / 2;    	
+    	heroes = heroes.subList(0, halfArray);
+    	
+    	 // TODO 6 : Loop throught the list and display the name of the remaining heroes
+    	 for (Hero heroName : heroes) {
+ 			System.out.println(heroName.getName());
+ 		}
     }
 }
